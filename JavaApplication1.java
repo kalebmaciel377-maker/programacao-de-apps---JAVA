2d36acf66b2be3c30efc24b15189c2434cb5f2ff
kalebmaciel377-maker <kalebmaciel377@gmail.com>
2026-09-02 14:02:38 -0300

    aula-1

---
+++ JavaApplication1.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author kaleb_m_santos
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("elias gay");
+        
+        
+        byte idade = 25;
+        
+        short anoNascimento = 2000;
+        
+        int quantidade = 100;
+        
+        long populcao = 212000000L;
+        
+        float altura = 1.75f;
+        
+        double salario = 3500.75;
+        
+        char sexo = 'M';
+        
+        boolean aprovado = true;
+        
+        String nome = "João";
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano de nascimento: " + anoNascimento);
+        System.out.println("Quantidade: " + quantidade);
+        System.out.println("População: " + populcao);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salario: " + salario);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Aprovado: " + aprovado);
+        System.out.println("Fim do programs!!!: ");
+        
+        float valor1 = 10.50f;
+        float valor2 = 25.75f;
+        
+        System.out.printf("valor 1: " + valor1);
+        System.out.format("valor 2: " + valor2);
+        
+        
+        
+        
+
+    }
+    
+}
