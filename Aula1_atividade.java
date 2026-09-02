9b0dc6de0e21f862e83c6813c0f9c2c5f7947d12
kalebmaciel377-maker <kalebmaciel377@gmail.com>
2026-09-02 14:25:52 -0300

    atividade_aula1

---
+++ Aula1_atividade.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula1_atividade;
+
+/**
+ *
+ * @author kaleb_m_santos
+ */
+public class Aula1_atividade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ferrari");
+        
+        String nome_produto = "Ferrari F40 (1987)";
+        
+        int codigo_produto = 123456;
+        
+        String categoria = "Esportivo";
+        
+        String marca = "Ferrari";
+        
+        float preco = 2.0000f;
+        
+        int qtd_estoque = 20;
+        
+        double peso = 2.100;
+        
+        boolean disponivel = true;
+        
+        System.out.println("Nome: " + nome_produto);
+        System.out.println("Codigo do carro: " + codigo_produto);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Marca: " + marca);
+        System.out.println("Preço: " + preco);
+        System.out.println("Quantidade em estoque: " + qtd_estoque);
+        System.out.println("Peso do carro: " + peso);
+        System.out.println("Está distponivel?: " + disponivel);
+
+    }
+    
+}
